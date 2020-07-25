4d87897f035076229bc9efd5c8d7970f41c2f75f
Melissa Moore <67612457+melissamoore1216@users.noreply.github.com>
2020-07-24 20:20:52 -0600

    Update function.js

--- function.js
+++ function.js
@@ -1,1 +1,27 @@
-window.onload = function sayHi(){ alert(“Hello World of Javascript!”); }
+<!--Image Grid (I am really stuck on how to know exactly what to use in the provided javascript W3 provided that will match up with what was used in my html specifically in my index.html-->
+<script>
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a loop variable
+var i;
+
+// Two images side by side
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "50%";  // IE10
+    elements[i].style.flex = "50%";
+  }
+}
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myHeader");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+</script>
