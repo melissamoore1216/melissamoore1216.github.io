2cea6652b4e1d6081b1855878227ffa6ae49d183
Melissa Moore <67612457+melissamoore1216@users.noreply.github.com>
2020-08-16 19:31:49 -0600

    Update function.js

--- function.js
+++ function.js
@@ -1,3 +1,8 @@
 document.getElementById("p2").style.color = "purple";
 document.getElementById("p2").style.fontFamily = "Arial";
 document.getElementById("p2").style.fontSize = "larger";
+function myFunction() {
+  var btn = document.createElement("BUTTON");
+  btn.innerHTML = "BOO YAH";
+  document.body.appendChild(btn);
+}
