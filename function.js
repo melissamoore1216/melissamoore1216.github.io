c8ac042bcaee3610c6a703bf219ec117c1bd795b
Melissa Moore <67612457+melissamoore1216@users.noreply.github.com>
2020-08-16 19:54:07 -0600

    Update function.js

--- function.js
+++ function.js
@@ -1,8 +1,11 @@
-document.getElementById("p2").style.color = "purple";
-document.getElementById("p2").style.fontFamily = "Arial";
-document.getElementById("p2").style.fontSize = "larger";
 function myFunction() {
   var btn = document.createElement("BUTTON");
   btn.innerHTML = "BOO YAH";
   document.body.appendChild(btn);
+}
+function myFunctionlook() {
+    document.getElementById("jsbuttonstyle").style.color = "magenta";
+}
+function myform() {
+    alert("I won't actually see it but I bet it's a great movie!");
 }
