376ca77ecde528ca5d54a9125dd8fc69a6d7c147
Melissa Moore <67612457+melissamoore1216@users.noreply.github.com>
2020-08-16 19:26:18 -0600

    Update function.js

--- function.js
+++ function.js
@@ -1,15 +1,3 @@
-var item1;
-var item2;
-var item3;
-document.getElementById("changeList").onclick = newList;
-function newList() {
-    item1 = prompt("Enter a new first thing: ");
-    item2 = prompt("Enter a new second thing: ");
-    item3 = prompt("Enter a new third thing: ");
-    updateList();
-}
-function updateList() {
-    document.getElementById("firstThing").innerHTML = item1;
-    document.getElementById("secondThing").innerHTML = item2;
-    document.getElementById("thirdThing").innerHTML = item3;
-}
+document.getElementById("p2").style.color = "purple";
+document.getElementById("p2").style.fontFamily = "Arial";
+document.getElementById("p2").style.fontSize = "larger";
